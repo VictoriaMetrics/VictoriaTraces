11046f538b521d42821b8dbc6fbcccd3c3e446ea
Jiekun <jiekun@victoriametrics.com>
2025-07-17 15:05:54 +0800

    test: [vtgen] update codes and readme

--- app/vtgen/README.md
+++ app/vtgen/README.md
@@ -2,14 +2,48 @@
 
 `vtgen` is a trace data generator. It reads the OTLP request body in `testdata/testdata.bin`, modifies the `trace_id`, `start_time` and `end_time`, and sends them to OTLP trace endpoint (`/v1/traces`).
 
+### Usage
+`vtgen` can be used for:
+1. Generating identical data for different targets, which is useful for query performance benchmarking of different storage backends against the same dataset.
+2. Generating different data at the same rate (on a best-effort basis) for different targets, which is useful for data ingestion performance benchmarking of different storage backends.
+
+To send identical data to different targets:
+```
+./vtgen -addrs=http://example-url1:port/v1/traces,http://example-url2:port/insert/opentelemetry/v1/traces
+```
+The performance of different targets will affect each other, as `vtgen` generates data and makes HTTP requests to them one by one.
+
+To send (potentially) different data to different addresses at the same rate, simply run multiple `vtgen` with different HTTP listening port:
+```
+./vtgen -addrs=http://example-url1:port/v1/traces -httpListenAddr=0.0.0.0:8080
+./vtgen -addrs=http://example-url2:port/insert/opentelemetry/v1/traces -httpListenAddr=0.0.0.0:8081
+```
+
+### Metrics
+`vtgen` exposes some metrics to help tracking the ingestion performance. Here's the example:
+```
+vt_gen_http_request_duration_seconds_bucket{path="http://example-url1:port/v1/traces",vmrange="7.743e-05...8.799e-05"} 8
+vt_gen_http_request_duration_seconds_bucket{path="http://example-url1:port/v1/traces",vmrange="3.162e-03...3.594e-03"} 1
+vt_gen_http_request_duration_seconds_sum{path="http://example-url1:port/v1/traces"} 0.17367758299999986
+vt_gen_http_request_duration_seconds_count{path="http://example-url1:port/v1/traces"} 667
+vt_gen_http_request_error_count{path="http://example-url1:port/v1/traces"} 667
+```
+
+- `vt_gen_http_request_duration_seconds_bucket/_sum/_count` is a `histogram` for each address.
+- `vt_gen_http_request_error_count` is a `counter` for each address.
+
 ### Command-line flags
 ```
--rate
-    Spans for each address per second.
-    The actual rate will be affected by each address, as the requests are sent synchronously to different addresses in a for-loop, 
-    to ensure that each address receive the same pressure. (default: 10000)
--addrs
-	OTLP trace export endpoints, split by `,`.
--authorizations
-    Basic auth value, split by `,`. If -authorizations is not empty, it must contains same items as -addrs.
+  -addrs string
+        otlp trace export endpoints, split by ",".
+  -authorizations string
+        authorization header for each -addrs, split by ",".
+  -httpListenAddr string
+        http listen address for pprof and metrics. (default "0.0.0.0:8080")
+  -logEvery10k int
+        how many trace id should be logged for every 10000 traces by each worker. (default 2)
+  -rate int
+        spans per second. (default 10000)
+  -worker int
+        number of workers. (default 4)
 ```
--- app/vtgen/main.go
+++ app/vtgen/main.go
@@ -20,104 +20,129 @@ import (
 
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/encoding/zstd"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/logger"
+	"github.com/VictoriaMetrics/metrics"
 	"golang.org/x/time/rate"
 
 	otelpb "github.com/VictoriaMetrics/VictoriaTraces/lib/protoparser/opentelemetry/pb"
 )
 
-var BodyList [][]byte
+var (
+	httpListenAddrs     = flag.String("httpListenAddr", "0.0.0.0:8080", "http listen address for pprof and metrics.")
+	spanRate            = flag.Int("rate", 10000, "spans per second.")
+	addrs               = flag.String("addrs", "", `otlp trace export endpoints, split by ",".`)
+	authHeaders         = flag.String("authorizations", "", `authorization headers for each -addrs, split by ",".`)
+	worker              = flag.Int("worker", 4, "number of workers.")
+	logNTraceIDEvery10K = flag.Int("logEvery10k", 2, "how many trace id should be logged for every 10000 traces by each worker.")
+)
 
 func main() {
-	spanRate := flag.Int("rate", 10000, "spans per second.")
-	addrs := flag.String("addrs", "", "otlp trace export endpoint.")
-	authHeaders := flag.String("authorizations", "", "authorization header.")
-	worker := flag.Int("worker", 4, "number of workers.")
-	logNTraceIDEvery10K := flag.Int("logEvery10k", 2, "how many trace id should be logged for every 10000 traces for each worker.")
+	// parse and validate cli flags.
+	addrList, authHeaderList := initFlags()
 
-	flag.Parse()
-	addrList := strings.Split(*addrs, ",")
-	for _, addr := range addrList {
-		if _, err := url.ParseRequestURI(addr); err != nil {
-			panic(fmt.Sprintf("invalid otlp trace export endpoint %s: %v", addr, err))
-		}
-	}
-	authHeaderList := strings.Split(*authHeaders, ",")
-	if *authHeaders != "" && len(addrList) != len(authHeaderList) {
-		panic("len(addrList) != len(authHeaderList)")
-	}
+	// load test data from file.
+	reqBodyList := loadTestData()
 
-	data, err := os.ReadFile("./app/vtgen/testdata/testdata.bin")
-	if err != nil {
-		panic(fmt.Sprintf("cannot read file %v", err))
-	}
-	var uncompressed []byte
-	uncompressed, err = zstd.Decompress(uncompressed, data)
-	if err != nil {
-		panic(fmt.Sprintf("cannot decompress %v", err))
-	}
-
-	gobDec := gob.NewDecoder(bytes.NewReader(uncompressed))
-	if err = gobDec.Decode(&BodyList); err != nil {
-		panic(fmt.Sprintf("cannot decode %v", err))
-	}
-
-	go http.ListenAndServe("0.0.0.0:8080", nil)
-
+	// rate limit
 	limiter := rate.NewLimiter(rate.Limit(*spanRate), *spanRate)
+
+	// create metrics and pprof HTTP server
+	requestHistogramList := make([]*metrics.Histogram, len(addrList))
+	errCountList := make([]*metrics.Counter, len(addrList))
+	for i, addr := range addrList {
+		requestHistogramList[i] = metrics.NewHistogram(`vt_gen_http_request_duration_seconds{path="` + addr + `"}`)
+		errCountList[i] = metrics.NewCounter(`vt_gen_http_request_error_count{path="` + addr + `"}`)
+	}
+	http.HandleFunc("/metrics", func(w http.ResponseWriter, req *http.Request) {
+		metrics.WritePrometheus(w, true)
+	})
+	go func() {
+		http.ListenAndServe(*httpListenAddrs, nil)
+	}()
+
 	wg := sync.WaitGroup{}
 	for i := 0; i < *worker; i++ {
 		wg.Add(1)
 		go func() {
 			for {
+				// The traceIDMap recorded old traceID->new traceID.
+				// Spans with same old traceID should be replaced with same new traceID.
 				traceIDMap := make(map[string]string)
+
+				// The timeOffset is the time offset of span timestamp and current timestamp.
+				// All spans' timestamp should be increased by this offset.
+				// This value should be initialized only once when iterating through the first span.
 				once := sync.Once{}
 				timeOffset := uint64(0)
-				for i := range BodyList {
-					data := BodyList[i]
+
+				// update the traceID and start_/end_timestamp of each span.
+				for i := range reqBodyList {
+					// the span count of this request. used for rate limiting.
+					spanCount := 0
+
+					// unmarshal binary request body to otelpb.ExportTraceServiceRequest
+					data := reqBodyList[i]
 					var req otelpb.ExportTraceServiceRequest
 					if err := req.UnmarshalProtobuf(data); err != nil {
 						panic(err)
 					}
-					spanCount := 0
+
+					// iterate all spans
 					for j := range req.ResourceSpans {
 						for k := range req.ResourceSpans[j].ScopeSpans {
 							spanCount += len(req.ResourceSpans[j].ScopeSpans[k].Spans)
+
 							for l := range req.ResourceSpans[j].ScopeSpans[k].Spans {
 								sp := req.ResourceSpans[j].ScopeSpans[k].Spans[l]
+
 								once.Do(func() {
 									timeOffset = uint64(time.Now().UnixNano()) - sp.StartTimeUnixNano
 								})
 								// replace TraceID
 								if tid, ok := traceIDMap[sp.TraceID]; ok {
+									// old traceID already seen. use the cached one.
 									sp.TraceID = tid
 								} else {
-									// generate one
-									h := md5.New()
-									h.Write([]byte(strconv.FormatInt(time.Now().UnixNano(), 10)))
-									traceID := hex.EncodeToString(h.Sum(nil))
-									traceIDMap[sp.TraceID] = traceID
+									// generate a new traceID by md5(timestamp) and put it into cache.
+									traceID := generateTraceID()
 									sp.TraceID = traceID
+									traceIDMap[sp.TraceID] = traceID
+
+									// log traceID for query test if needed.
 									if rand.Intn(10000) < *logNTraceIDEvery10K {
 										logger.Infof(traceID)
 									}
 								}
+
+								// adjust the timestamp of the span.
 								sp.StartTimeUnixNano = sp.StartTimeUnixNano + timeOffset
 								sp.EndTimeUnixNano = sp.EndTimeUnixNano + timeOffset + uint64(rand.Int63n(100000000))
 							}
 						}
 					}
+
+					// rate limit
 					limiter.WaitN(context.TODO(), spanCount)
+
+					// send request to each address.
 					for addrIdx, addr := range addrList {
+						// prepare request.
 						httpReq, err := http.NewRequest("POST", addr, bytes.NewReader(req.MarshalProtobuf(nil)))
 						if *authHeaders != "" {
 							httpReq.Header.Add("authorization", authHeaderList[addrIdx])
 						}
 						httpReq.Header.Add("content-type", "application/x-protobuf")
+
+						// do request and record metrics.
+						startTime := time.Now()
 						res, err := http.DefaultClient.Do(httpReq)
 						if err != nil {
 							logger.Errorf("trace export error: %s", err)
+							errCountList[addrIdx].Add(1)
 						}
-						res.Body.Close()
+						if res != nil {
+							res.Body.Close()
+						}
+						requestHistogramList[addrIdx].Update(time.Now().Sub(startTime).Seconds())
 					}
 				}
 			}
@@ -134,20 +159,67 @@ func main() {
 //
 // You have to prepare the request body binary in advance.
 func readWrite() {
+	var bodyList [][]byte
 	for i := 0; i <= 100; i++ {
 		dat, err := os.ReadFile(fmt.Sprintf("%d.bin", i))
 		if err != nil {
 			panic(fmt.Sprintf("cannot read file %d: %v", i, err))
 		}
-		BodyList = append(BodyList, dat)
+		bodyList = append(bodyList, dat)
 	}
 
 	var buf bytes.Buffer
 	gobEnc := gob.NewEncoder(&buf)
-	if err := gobEnc.Encode(BodyList); err != nil {
+	if err := gobEnc.Encode(bodyList); err != nil {
 		panic(err)
 	}
 	var compressed []byte
 	compressed = zstd.CompressLevel(compressed, buf.Bytes(), 3)
 	os.WriteFile("./app/vtgen/testdata/testdata.bin", compressed, 0666)
+}
+
+func initFlags() ([]string, []string) {
+	flag.Parse()
+	addrList := strings.Split(*addrs, ",")
+	for _, addr := range addrList {
+		if _, err := url.ParseRequestURI(addr); err != nil {
+			panic(fmt.Sprintf("invalid otlp trace export endpoint %s: %v", addr, err))
+		}
+	}
+	authHeaderList := strings.Split(*authHeaders, ",")
+	if *authHeaders != "" && len(addrList) != len(authHeaderList) {
+		panic("len(addrList) != len(authHeaderList)")
+	}
+	return addrList, authHeaderList
+}
+
+func loadTestData() [][]byte {
+	var bodyList [][]byte
+
+	// read compressed binary data
+	data, err := os.ReadFile("./app/vtgen/testdata/testdata.bin")
+	if err != nil {
+		panic(fmt.Sprintf("cannot read file %v", err))
+	}
+
+	// decompress
+	var uncompressed []byte
+	uncompressed, err = zstd.Decompress(uncompressed, data)
+	if err != nil {
+		panic(fmt.Sprintf("cannot decompress %v", err))
+	}
+
+	// unmarshal binary data to the slice of request body ([]byte)
+	gobDec := gob.NewDecoder(bytes.NewReader(uncompressed))
+	if err = gobDec.Decode(&bodyList); err != nil {
+		panic(fmt.Sprintf("cannot decode %v", err))
+	}
+
+	return bodyList
+}
+
+func generateTraceID() string {
+	h := md5.New()
+	h.Write([]byte(strconv.FormatInt(time.Now().UnixNano(), 10)))
+	return hex.EncodeToString(h.Sum(nil))
 }
