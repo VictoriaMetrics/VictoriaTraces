ec7ba22d20fee577e4c964406c1bc8c08291708e
Jiekun <jiekun@victoriametrics.com>
2026-07-06 10:14:20 +0800

    vtselect: unify lookbehind window for tag-related search APIs

--- app/vtselect/traces/tempo/tempo.go
+++ app/vtselect/traces/tempo/tempo.go
@@ -708,7 +708,7 @@ func parseTempoAPIParam(_ context.Context, r *http.Request, allowDefaultTime boo
 	}
 
 	if allowDefaultTime {
-		p.start = time.Now().Add(-10 * time.Minute)
+		p.start = time.Now().Add(-*tracecommon.TraceTagsLookbehind)
 		p.end = time.Now()
 	}
 
